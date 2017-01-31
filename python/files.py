edde1e5d8a1074a40f939c547d063b5422f0de74
Matt <mwiddup>
2017-01-31 02:36:47 +0000

    file.py is mostly working now. Might need to do some additional checks for missing files

--- python/files.py
+++ python/files.py
@@ -13,9 +13,11 @@ and match. Anything inconsistant on the file path side (files exist that aren't
 in the install*.sh files) are reported as an error
 Output should conform to Junit standards; natively or by using go-junit-report
 """
-
+import sys
 from os import walk
 from os.path import join
+from re import compile, match, search 
+from timeit import default_timer as time
 
 def get_filepaths(directory):
     file_paths = []
@@ -27,8 +29,55 @@ def get_filepaths(directory):
 
     return file_paths
 
-full_file_paths = get_filepaths("/home/vagrant/repo/CR19321")
+def grep_clean(file_list):
+    expected = []
+    pat = compile('".*\s(.*)"')
+    
+    for file in installers:
+         for line in open(file, 'r'):
+             if search(pattern, line):
+                 m = search(pat, line)
+                 expected.append(m.group(1))
+
+    return expected
+
+root_path = str(sys.argv[1])
+pattern = str(sys.argv[2])
+
+stime = time()
+full_file_paths = get_filepaths(root_path)
+
+installers = []
+objects = []
+
+instFile = compile(".*install.*\.sh$")
+junkFile = compile("^\.*svn")
 
 for f in full_file_paths:
-   print f
+    if match(instFile, f):
+        installers.append(f)
+    elif match(junkFile, f.split(root_path,1).pop()):
+      """Nothing""" 
+    else:
+        objects.append(f.split(root_path,1).pop())
 
+expected = grep_clean(installers)
+
+difference = (set(objects) - set(expected))
+etime = time() - stime
+
+print "=== RUN   Extra Files"
+if len(difference) > 0:
+    print "--- FAIL: Extra Files ("+str(round(etime,2))+" seconds):The following files are in the tag, not in an installer"
+    for t in sorted(difference):
+       print '\t' + t
+    print "FAIL"
+    print "exit status 1"
+    print "FAIL\tExtra Files\t"+str(round(etime,3))+"s"
+
+"""
+print "Number of objects: " + str(len(objects))
+print "Number of installers: " + str(len(installers))
+print "Number of expected: " + str(len(expected))
+print "Number of difference: " + str(len(difference))
+"""
