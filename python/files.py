f55b2b3a186ab60200ec46288729d91e17a4a737
Matt <mwiddup>
2017-02-16 05:54:24 +0000

    Recheck the list of missing files against the installers. Now with more inline comments

--- python/files.py
+++ python/files.py
@@ -19,6 +19,10 @@ from os.path import join
 from re import compile, match, search 
 from timeit import default_timer as time
 
+"""
+Grabs all files from all directories specified at the root location
+No filtering at this stage, just a full list
+"""
 def get_filepaths(directory):
     file_paths = []
 
@@ -29,11 +33,16 @@ def get_filepaths(directory):
 
     return file_paths
 
+
+"""
+Returns a list of all patterns matched, cleaning them in the process by
+pulling out just the file path to the object
+"""
 def grep_clean(file_list):
     expected = []
     pat = compile('".*\s(.*)"')
     
-    for file in installers:
+    for file in file_list:
          for line in open(file, 'r'):
              if search(pattern, line):
                  m = search(pat, line)
@@ -41,6 +50,28 @@ def grep_clean(file_list):
 
     return expected
 
+
+"""
+Returns a list of matched patterns alone, based on the previous list of
+matched patterns. Second run through the installers looking for specific
+file mentions. This should capture code listed in the installer that is not
+using the DB2_Run command
+"""
+def grep_leftovers(file_list, missing_list):
+    cleaned = []
+
+    for thingy in missing_list:
+        for file in file_list:
+            for line in open(file, 'r'):
+                if search(thingy, line):
+                    cleaned.append(thingy)
+                    break #one instance of a matched pattern(thingy) is enough
+
+    return cleaned
+
+"""
+Grabs the input values
+"""
 root_path = str(sys.argv[1])
 pattern = str(sys.argv[2])
 
@@ -51,8 +82,12 @@ installers = []
 objects = []
 
 instFile = compile(".*install.*\.sh$")
-junkFile = compile("^\.*svn")
+junkFile = compile("^\.\w")
 
+"""
+Go through some of the gathered lists of files and filter out those that
+are needed for further tests. Reject the others
+"""
 for f in full_file_paths:
     if match(instFile, f):
         installers.append(f)
@@ -63,7 +98,13 @@ for f in full_file_paths:
 
 expected = grep_clean(installers)
 
+"""
+Compare two lists, the left overs are those that are missing from installers
+The second list comparison runs over the remaining object through the files
+to ensure their not used
+"""
 difference = (set(objects) - set(expected))
+difference = (set(difference) - set(grep_leftovers(installers, difference)))
 etime = time() - stime
 
 print "=== RUN   Extra Files"
